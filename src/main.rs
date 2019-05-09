eaaa248a98cac8e3a11835efc0327c26654f27bd
jonathan gerber <jlgerber@gmail.com>
2019-05-08 22:29:46 -0700

    updatees

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,40 @@
 use gpi::GPI;
 use gpi::VcsType;
-use std::env;
 use gpi::table_record;
 
-use std::path::PathBuf;
 use structopt::StructOpt;
+
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "gpi", about = "global package index")]
 enum GpiCmd {
+    /// List the packages in the Global Package Index.
+    #[structopt(name = "list")]
+    List,
+    /// Query for a package in global package index
     #[structopt(name = "query")]
     Query {
+        /// The name of the package to search for
         #[structopt(name = "PACKAGE")]
         package: String,
+        #[structopt(short = "t", long="table")]
+        table: bool,
+         #[structopt(short = "j", long="json")]
+        json: bool,
+         #[structopt(short = "p", long="pretty-json")]
+        pjson: bool,
     },
+    /// Create a new package in the global package index
     #[structopt(name = "new")]
     New {
+        /// The name of the source code management system used (git | svn)
+         #[structopt(short = "s", long="scm")]
+        scm: VcsType,
+        /// The name of the package to create
         #[structopt(name = "PACKAGE")]
         package: String,
     },
+    /// Add a repo for a package in the global package index
     #[structopt(name = "add")]
     Add {
         #[structopt(short = "s", long="scm")]
@@ -31,15 +47,24 @@ enum GpiCmd {
 
 fn process_cmds() -> Result<(), gpi::GpiError> {
     match GpiCmd::from_args() {
-        GpiCmd::Query {package } => {
-            let pkg = GPI::get_record(&package)?;
-            table_record(&package, &pkg);
+        GpiCmd::List => {
+            // ge
         },
-        GpiCmd::New { package,  } => {
-            println!("create {} Not Supported Yet", package);
+        GpiCmd::Query {package, table, json, pjson } => {
+            let pkg = GPI::get_record(&package)?;
+            if table || (!table && !json && !pjson) {
+                table_record(&package, &pkg);
+            } else if json {
+                println!("{:?}", pkg);
+            } else if pjson {
+                println!("{:#?}", pkg);
+            }
+        },
+        GpiCmd::New { scm, package,  } => {
+            println!("Creating the package named '{}' in the '{}' source code management system is not supported yet",  package, scm);
         },
         GpiCmd::Add { scm, package  } => {
-            println!("create {} {} Not Supported Yet", scm, package);
+            println!("Adding '{}' to the '{}' record in the GPI not supported yet", scm, package);
         }
     }
     Ok(())
