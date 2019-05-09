d33d04742285b91e6e08fc52869e08067ba58652
jonathan gerber <jlgerber@gmail.com>
2019-05-08 22:08:39 -0700

    adds structopt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,50 @@
 use gpi::GPI;
+use gpi::VcsType;
 use std::env;
 use gpi::table_record;
 
-fn main() -> Result<(), gpi::GpiError> {
-    let mut args = env::args();
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-    if let Some(package) = args.nth(1) {
-        let pkg = GPI::get_record(&package)?;
-        table_record(&package, &pkg);
-    } else {
-        println!("please supply a package name");
-    };
+#[derive(StructOpt, Debug)]
+#[structopt(name = "gpi", about = "global package index")]
+enum GpiCmd {
+    #[structopt(name = "query")]
+    Query {
+        #[structopt(name = "PACKAGE")]
+        package: String,
+    },
+    #[structopt(name = "new")]
+    New {
+        #[structopt(name = "PACKAGE")]
+        package: String,
+    },
+    #[structopt(name = "add")]
+    Add {
+        #[structopt(short = "s", long="scm")]
+        scm: VcsType,
+
+        #[structopt(name = "PACKAGE")]
+        package: String,
+    }
+}
+
+fn process_cmds() -> Result<(), gpi::GpiError> {
+    match GpiCmd::from_args() {
+        GpiCmd::Query {package } => {
+            let pkg = GPI::get_record(&package)?;
+            table_record(&package, &pkg);
+        },
+        GpiCmd::New { package,  } => {
+            println!("create {} Not Supported Yet", package);
+        },
+        GpiCmd::Add { scm, package  } => {
+            println!("create {} {} Not Supported Yet", scm, package);
+        }
+    }
     Ok(())
+}
 
+fn main() -> Result<(), gpi::GpiError> {
+    process_cmds()
 }
